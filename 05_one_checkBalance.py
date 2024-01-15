d80a275f509f8a98674d393ae367b586d99bbd39
houyuan <91812763+xyyz12@users.noreply.github.com>
2024-01-15 11:42:10 +0800

    Update 05_one_checkBalance.py

--- 05_one_checkBalance.py
+++ 05_one_checkBalance.py
@@ -11,8 +11,10 @@ except:
 	walletAddress = '0x2C2E5b824EA2BcE625943aF15e1bBD86630B37EF'
 
 
+I = "http://127.0.0.1:xxxx"
 
-w3 = Web3(Web3.HTTPProvider(infuraUrl))
+w3 = Web3(Web3.HTTPProvider(infuraUrl, request_kwargs={"proxies":{'https' : I, 'http' : I }}))
+#w3 = Web3(Web3.HTTPProvider(infuraUrl))
 
 
 def fetchETHBalance(walletAddress):
